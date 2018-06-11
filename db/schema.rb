a1c2ef978167d8c7d3694a5c221d24828ad1e0b1
Henrique Silva <hbs003@hotmail.com>
2018-06-11 17:45:15 +0200

    migrate pending done

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_11_144801) do
+ActiveRecord::Schema.define(version: 2018_06_11_152223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "period"
+    t.float "total"
+    t.string "status"
+    t.integer "guests"
+    t.bigint "user_id"
+    t.bigint "flat_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flat_id"], name: "index_bookings_on_flat_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "flats", force: :cascade do |t|
     t.string "name"
@@ -44,5 +57,7 @@ ActiveRecord::Schema.define(version: 2018_06_11_144801) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "flats"
+  add_foreign_key "bookings", "users"
   add_foreign_key "flats", "users"
 end
