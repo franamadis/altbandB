bd658cb9e14671848d46a3b8b74207f42ee3e89b
Fran Amadis <info@franamadis.com>
2018-06-14 12:50:11 +0200

    css improvements home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,15 @@
 class PagesController < ApplicationController
 
   def home
-    @flats = Flat.all
+    @flats = Flat.where.not(latitude: nil, longitude: nil)
+
+    @markers = @flats.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude#,
+        # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
+      }
+    end
   end
+    # @flats = Flat.all
 end
