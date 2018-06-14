f92499be1d4cede872674cbe122114ee4bdd9aff
David Martins <davidvicentemartins@gmail.com>
2018-06-14 11:56:22 +0200

    Added validations for flat, user

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -3,4 +3,14 @@ class Flat < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   mount_uploader :photo, PhotoUploader
+
+  validates :name, uniqueness: true
+  validates :name, presence: true
+  validates :address, presence: true
+  # validates :address, uniqueness: true
+  validates :description, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validates :max_guests, numericality: {greater_than_or_equal_to: 0 }
+
 end
+
